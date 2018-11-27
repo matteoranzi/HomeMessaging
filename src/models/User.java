b8936372575e8c3de11420e0d4b834840771b084
Matteo Ranzi <matteoranzi2000@gmail.com>
2018-11-27 23:55:48 +0100

    Basic definition of user class (information required for any host)

--- src/models/User.java
+++ src/models/User.java
@@ -7,5 +7,51 @@ package models;
  * Time: 0.41
  */
 public class User {
+    static private final String DEFAULT_CHAT_CODE = "ABCDEFG";
 
+    private String username;
+    private String appVersion;
+    private String chatCode;
+    private String deviceInfo;
+    private int listeningPort;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAppVersion() {
+        return appVersion;
+    }
+
+    public void setAppVersion(String appVersion) {
+        this.appVersion = appVersion;
+    }
+
+    public String getChatCode() {
+        return chatCode;
+    }
+
+    public void setChatCode(String chatCode) {
+        this.chatCode = chatCode;
+    }
+
+    public String getDeviceInfo() {
+        return deviceInfo;
+    }
+
+    public void setDeviceInfo(String deviceInfo) {
+        this.deviceInfo = deviceInfo;
+    }
+
+    public int getListeningPort() {
+        return listeningPort;
+    }
+
+    public void setListeningPort(int listeningPort) {
+        this.listeningPort = listeningPort;
+    }
 }
